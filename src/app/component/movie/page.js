fa2c3515c9897256ebbc7de9deea5a232f28ba5a
coderkannaujia <kumamit482000@gmail.com>
2024-02-22 19:38:32 +0530

    files changes

---
+++ src/app/component/movie/page.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { FaGithub } from "react-icons/fa";
+import { AiOutlineCaretRight } from "react-icons/ai";
+import { BiChevronLeft } from "react-icons/bi";
+import movie from "/public/image/movie.png"
+
+const Movie = () => {
+ return (
+  <div className="h-fit w-full text-center">
+    <div className="max-w-screen-xl mx-auto w-full h-full pt-24 p-8 flex flex-col">
+      <div className="flex">
+       <Link href="/component/portfolio">
+          <div className="flex items-center justify-center my-8 text-indigo-500 font-bold capitalize cursor-pointer">
+            <BiChevronLeft size={25} /> back
+          </div>
+        </Link>
+      </div>
+
+      <h1 className="capitalize text-4xl mt-2 mb-8 text-center md:text-left font-bold text-blue-500 tracking-wider">
+        food
+      </h1>
+
+      <div className="relative w-96 h-56 mx-auto overflow-hidden my-8">
+        <Image src={movie} alt="bla" layout="fill" objectFit="cover" />
+      </div>
+
+      <h2 className="text-center md:text-left my-4 text-2xl font-bold">
+        Description
+      </h2>
+
+      <p>
+        Human thy god sainted is the distinctly shrieked grave bird shrieked,
+        yore borrow nevermore but gaunt maiden have nothing darkness, answer
+        evermore word there from raven, sorrowsorrow from on and nevermore of
+        core. Whom the meant and while i of respiterespite his lamplight. Or
+        being weary parting grave lent if. Rare came stillness whether the
+        lord. Bust myself whom of soul silken. The that there this my, quaint
+        that only lenore stern muttered and nothing or tapping. Here lie
+        lining i still. A tis some and chamber flitting. Lamplight radiant
+        from no yet what, upon that that one decorum leave.
+      </p>
+
+      <div className="flex items-center justify-center gap-10">
+        <Link href="https://amitkmoviewebsite.netlify.app/">
+          <div className="group flex items-center justify-center my-8 bg-blue-500 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer">
+            demo
+            <span className="duration-200 ease-in">
+              <AiOutlineCaretRight size={18} className="ml-2" />
+            </span>
+          </div>
+        </Link>
+
+        <Link href="https://github.com/coderkannaujia">
+          <div className="group flex items-center justify-center my-8 bg-blue-500 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer">
+            my portfolio
+            <span className="duration-200 ease-in">
+              <FaGithub size={18} className="ml-2" />
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  </div>
+ )
+}
+
+export default Movie
--- src/app/component/portfolio/page.js
+++ src/app/component/portfolio/page.js
@@ -1,89 +1,192 @@
-import Link from 'next/link';
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 import { MdExpandMore } from "react-icons/md";
-import Image from 'next/image';
-
 import food from "/public/image/food.png"
-import travel from "/public/image/travel.png"
+import educaction from "/public/image/education.png"
 import movie from "/public/image/movie.png"
-import education from "/public/image/education.png"
-
-
-const Portfolio = () => {
-
-      const portfolio =[
-          {
-            id:1,
-            title:"nextjs food app", 
-            imageSrc:food,
-            url:"food-app"
-          },
-          {
-           id:2,
-           title:"travel-app",
-           imageSrc:travel,
-           url:"travel-app"
-         },
-         {
-          id:3,
-          title:"movie-react-app",
-          imageSrc:movie,
-          url:"movie-react-app"
-        },
-        {
-         id:4,
-         title:"education-next-app",
-         imageSrc:education,
-         url:"education-next-app"
-       }
-      ]
+import travel from "/public/image/travel.png"
 
 
 
-  return (
-    <div id='portfolio' className='w-full'>
-     <div className='max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left'>
-      <h2 className='text-5xl md:text-7xl tracking-wider uppercase text-blue-500 font-bold '>portfolio</h2>
-      <p className='py-4 text-xl font-bold'>
-      A portfolio is a snapshot of one's accomplishments, skills, experiences, and projects, meticulously curated to showcase one's abilities and potential. It serves as a dynamic representation of growth and proficiency, illustrating a journey of continuous learning and development. 
+
+const Porfolio = () => {
+
+
+return (
+  <div id="portfolio" className="w-full">
+    <div className="max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left">
+      <h2 className="text-5xl md:text-7xl tracking-wider uppercase text-blue-500 font-bold">
+        portfolio
+      </h2>
+      <p className="py-4 max-w-lg">
+        While only that hauntedtell many purple no lent cushions more heart.
+        Soul cushions the still if i, hath lamplight mortals the rapping have
+        me and.
       </p>
-      
-      {/* /portfolio/installNode.jpg */}
-      <div className='max-w-5xl mx-auto grid md:grid-cols-2 gap-8'>
-             {portfolio.map(({id ,title,imageSrc ,url})=>(
-                 <Link key={id} href='youtube.com'> 
-                  <div className='cursor-pointer group shadow-md shadow-gray-600 overflow-hidden rounded-md '>
-                   <Image 
-                   src={imageSrc}
-                   alt={title}
-                   className='rounded-md duration-200 hover:scale-110' />
-                   <h2 className='text-center text-base capitalize my-4 font-light duration-200 group-hover:underline underline-offset-4'>
-                   
+
+      <div className="max-w-5xl mx-auto grid md:grid-cols-2 gap-8">
+        
+          <Link  href="/component/food"> 
+            <div className="cursor-pointer group shadow-md shadow-gray-600 overflow-hidden rounded-md">
+              <Image
+                src={food}
+                alt="food-app"
+                className="rounded-md duration-200 
+                hover:scale-110"
+              />
+              <h2 className="text-center text-base capitalize my-4 font-bold duration-200 group-hover:underline underline-offset-4"> food-app </h2>
+            </div>
+          </Link>
         
 
-                  </h2>
-                  </div>
-                 </Link>
-              ))}
-              </div>
 
-          <div className='flex items-center justify-center'>
-          <Link href="/#portfolio">
-          <div
-            className="group  flex items-center justify-center my-8 bg-blue-500 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer"
-         >
+
+          <Link  href="/component/education"> 
+            <div className="cursor-pointer group shadow-md shadow-gray-600 overflow-hidden rounded-md">
+              <Image
+                src={educaction}
+                alt="food-app"
+                className="rounded-md duration-200 
+                hover:scale-110"
+              />
+              <h2 className="text-center text-base capitalize my-4 font-bold duration-200 group-hover:underline underline-offset-4"> educaction-app </h2>
+            </div>
+          </Link>
+
+
+
+          <Link  href="/component/movie"> 
+            <div className="cursor-pointer group shadow-md shadow-gray-600 overflow-hidden rounded-md">
+              <Image
+                src={movie}
+                alt="food-app"
+                className="rounded-md duration-200 
+                hover:scale-110"
+              />
+              <h2 className="text-center text-base capitalize my-4 font-bold duration-200 group-hover:underline underline-offset-4"> movie-app </h2>
+            </div>
+          </Link>
+
+
+          <Link  href="/component/travel"> 
+            <div className="cursor-pointer group shadow-md shadow-gray-600 overflow-hidden rounded-md">
+              <Image
+                src={travel}
+                alt="food-app"
+                className="rounded-md duration-200 
+                hover:scale-110"
+              />
+              <h2 className="text-center text-base capitalize my-4 font-bold duration-200 group-hover:underline underline-offset-4"> travel-app </h2>
+            </div>
+          </Link>
+      </div>
+
+
+    
+      <div className="flex items-center justify-center">
+        <Link href="/component/portfolio">
+          <div className="group flex items-center justify-center my-8 bg-blue-500 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer">
             all projects
             <span className="-rotate-90 duration-100 ease-in group-hover:translate-x-5">
               <MdExpandMore size={25} />
             </span>
           </div>
-          </Link>
-          </div>
-
-
+        </Link>
       </div>
     </div>
-  )
-}
+  </div>
+);
+};
 
-export default Portfolio
+
+export default Porfolio
+// import Link from 'next/link';
+// import React from 'react'
+// import { MdExpandMore } from "react-icons/md";
+// import Image from 'next/image';
+
+// import food from "/public/image/food.png"
+// import travel from "/public/image/travel.png"
+// import movie from "/public/image/movie.png"
+// import education from "/public/image/education.png"
+
+
+// const Portfolio = () => {
+
+//       const portfolio =[
+//           {
+//             id:1,
+//             title:"nextjs food app", 
+//             imageSrc:food,
+//             url:"food-app"
+//           },
+//           {
+//            id:2,
+//            title:"travel-app",
+//            imageSrc:travel,
+//            url:"travel-app"
+//          },
+//          {
+//           id:3,
+//           title:"movie-react-app",
+//           imageSrc:movie,
+//           url:"movie-react-app"
+//         },
+//         {
+//          id:4,
+//          title:"education-next-app",
+//          imageSrc:education,
+//          url:"education-next-app"
+//        }
+//       ]
+
+
+
+//   return (
+//     <div id='portfolio' className='w-full'>
+//      <div className='max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left'>
+//       <h2 className='text-5xl md:text-7xl tracking-wider uppercase text-blue-500 font-bold '>portfolio</h2>
+//       <p className='py-4 text-xl font-bold'>
+//       A portfolio is a snapshot of one's accomplishments, skills, experiences, and projects, meticulously curated to showcase one's abilities and potential. It serves as a dynamic representation of growth and proficiency, illustrating a journey of continuous learning and development. 
+//       </p>
+      
+//       {/* /portfolio/installNode.jpg */}
+//       <div className='max-w-5xl mx-auto grid md:grid-cols-2 gap-8'>
+//              {portfolio.map(({id ,title,imageSrc ,url})=>(
+//                  <Link key={id} href='youtube.com'> 
+//                   <div className='cursor-pointer group shadow-md shadow-gray-600 overflow-hidden rounded-md '>
+//                    <Image 
+//                    src={imageSrc}
+//                    alt={title}
+//                    className='rounded-md duration-200 hover:scale-110' />
+//                    <h2 className='text-center text-base capitalize my-4 font-light duration-200 group-hover:underline underline-offset-4'>
+                   
+        
+
+//                   </h2>
+//                   </div>
+//                  </Link>
+//               ))}
+//               </div>
+
+//           <div className='flex items-center justify-center'>
+//           <Link href="/#portfolio">
+//           <div
+//             className="group  flex items-center justify-center my-8 bg-blue-500 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer"
+//          >
+//             all projects
+//             <span className="-rotate-90 duration-100 ease-in group-hover:translate-x-5">
+//               <MdExpandMore size={25} />
+//             </span>
+//           </div>
+//           </Link>
+//           </div>
+
+
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Portfolio
